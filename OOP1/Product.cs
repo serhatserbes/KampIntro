9850aaf826ec04be0e0e36399aceb2ee856d1f03
Serhat Serbes <serhatserbes45@gmail.com>
2021-01-21 00:01:54 +0300

    20012021

---
+++ OOP1/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        //kategori 
+        public string ProductName { get; set; }
+        //ürün adı
+        public double UnitPrice { get; set; }
+        //birim fiyatı
+        public int UnitsInStock { get; set; }
+
+
+
+
+    }
+}
